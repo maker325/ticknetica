3fd72c3d33abfade5585842da844b6386a391785
maker325 <maker325@gmail.com>
2018-02-01 07:25:25 +0300

    lesson 1 fix2

--- part2/task3.rb
+++ part2/task3.rb
@@ -4,26 +4,15 @@ b1 = gets.chomp.to_f
 c1 = gets.chomp.to_f
 
 sides = [a1, b1, c1].sort!
-a = sides[0]
-b = sides[1]
-c = sides[2]
+cathetus1 = sides[0]
+cathetus2 = sides[1]
+hypotenuse = sides[2]
 
-case1 = c**2 == a**2 + b**2
-case2 = (a == b) && (b == c)
-case3 = (a == b) || (a == c) || (b == c)
+case1 = hypotenuse**2 == cathetus1**2 + cathetus2**2
+case2 = (cathetus1 == cathetus2) && (cathetus2 == hypotenuse)
+case3 = (cathetus1 == cathetus2) || (cathetus1 == hypotenuse) || (cathetus2 == hypotenuse)
 
-if case1
-  puts "Треугольник прямоугольный"
-end
-
-if case2
-  puts "Треугольник равнобедренный и равноcторонний"
-end
-
-if case3 && !case2
-  puts "Треугольник равнобедренный"
-end
-
-if !((case1) || (case3))
-  puts "Треугольник обычный"
-end
+puts "Треугольник прямоугольный" if case1
+puts "Треугольник равнобедренный и равноcторонний" if case2
+puts "Треугольник равнобедренный" if case3 && !case2
+puts "Треугольник обычный" if !((case1) || (case3))
