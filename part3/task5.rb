454f22aa184828639b606c1d9c00890864739d16
maker325 <maker325@gmail.com>
2018-02-06 20:19:40 +0300

    lesson 3 fix3

--- part3/task5.rb
+++ part3/task5.rb
@@ -7,7 +7,7 @@ puts 'Enter year:'
 year = gets.chomp.to_i
 
 amount_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-amount_of_days[1] = 29 if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+amount_of_days[1] = 29 if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 number = 0
 
 for x in (0..month - 2)
